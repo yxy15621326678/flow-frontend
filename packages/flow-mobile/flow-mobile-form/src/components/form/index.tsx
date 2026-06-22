09e163623f8b9a8a9fecc8230603f329def45db4
yanxiangyang <1017633908@qq.com>
2026-06-22 22:19:59 +0800

    测试合并

--- packages/flow-mobile/flow-mobile-form/src/components/form/index.tsx
+++ packages/flow-mobile/flow-mobile-form/src/components/form/index.tsx
@@ -1,39 +1,38 @@
+import { FlowList } from "@/components/list";
+import { FormViewProps } from "@coding-flow/flow-types";
 import React from "react";
-import {FormViewProps} from "@coding-flow/flow-types";
-import {FlowFormView as FormView} from "./view";
-import {FlowList} from "@/components/list";
+import { FlowFormView as FormView } from "./view";
 
 export const FlowFormView: React.FC<FormViewProps> = (props) => {
+  const form = props.form;
 
-    const form = props.form;
+  const formList = props.formList || [];
 
-    const formList = props.formList || [];
+  if (props.mergeable) {
+    return (
+      <FlowList
+        formList={formList}
+        initData={props.initData}
+        meta={props.meta}
+        onValuesChange={props.onValuesChange}
+        review={props.review}
+        fieldPermissions={props.fieldPermissions}
+        onMergeRecordIdsSelected={props.onMergeRecordIdsSelected}
+      />
+    );
+  }
 
-    if(props.mergeable){
-        return (
-            <FlowList
-                formList={formList}
-                initData={props.initData}
-                meta={props.meta}
-                onValuesChange={props.onValuesChange}
-                review={props.review}
-                fieldPermissions={props.fieldPermissions}
-                onMergeRecordIdsSelected={props.onMergeRecordIdsSelected}
-            />
-        )
-    }
-
-    if(form) {
-        return (
-            <FormView
-                form={form}
-                data={props.data}
-                meta={props.meta}
-                initData={props.initData}
-                onValuesChange={props.onValuesChange}
-                review={props.review}
-                fieldPermissions={props.fieldPermissions}
-            />
-        )
-    }
+  if (form) {
+    return (
+      <FormView
+        form={form}
+        data={props.data}
+        meta={props.meta}
+        initData={props.initData}
+        onValuesChange={props.onValuesChange}
+        review={props.review}
+        fieldPermissions={props.fieldPermissions}
+      />
+    );
+  }
 };
