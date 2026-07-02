9a2fa8c8f77b30173382142ba47fb8cfae9c45df
yanxiangyang <1017633908@qq.com>
2026-07-02 17:06:09 +0800

    抽屉默认行为去除

--- packages/flow-pc/flow-pc-approval/src/components/flow-approval/view.tsx
+++ packages/flow-pc/flow-pc-approval/src/components/flow-approval/view.tsx
@@ -1,70 +1,71 @@
+import { detail } from "@/api/record";
+import { ApprovalLayout } from "@/components/flow-approval/layout";
+import {
+  ApprovalPanelProps,
+  useMockContext,
+} from "@coding-flow/flow-approval-presenter";
+import { Drawer } from "@coding-flow/flow-pc-ui";
+import { FlowContent } from "@coding-flow/flow-types";
 import React from "react";
-import {Drawer} from "@coding-flow/flow-pc-ui";
-import {detail} from "@/api/record";
-import {FlowContent} from "@coding-flow/flow-types";
-import {ApprovalLayout} from "@/components/flow-approval/layout";
-import {useMockContext} from "@coding-flow/flow-approval-presenter";
-import {ApprovalPanelProps} from "@coding-flow/flow-approval-presenter";
 
 export const ApprovalPanel: React.FC<ApprovalPanelProps> = (props) => {
+  const [content, dispatch] = React.useState<FlowContent | undefined>(
+    undefined,
+  );
 
-    const [content, dispatch] = React.useState<FlowContent | undefined>(undefined);
+  const mockKey = useMockContext();
 
-    const mockKey = useMockContext();
+  React.useEffect(() => {
+    const id = props.recordId || props.workflowCode || "";
+    detail(id, mockKey).then((res) => {
+      if (res.success) {
+        dispatch(res.data);
+      }
+    });
+  }, []);
 
-    React.useEffect(() => {
-        const id = props.recordId || props.workflowCode || '';
-        detail(id,mockKey).then(res => {
-            if (res.success) {
-                dispatch(res.data);
-            }
-        });
-    }, []);
-
-    return (
-        <div>
-            {content &&
-                (
-                    <ApprovalLayout
-                        className={props.className}
-                        content={content}
-                        onClose={props.onClose}
-                        review={props.review}
-                        initData={props.initData}
-                    />
-                )
-            }
-        </div>
-    )
-}
+  return (
+    <div>
+      {content && (
+        <ApprovalLayout
+          className={props.className}
+          content={content}
+          onClose={props.onClose}
+          review={props.review}
+          initData={props.initData}
+        />
+      )}
+    </div>
+  );
+};
 
 interface ApprovalPanelDrawerProps extends ApprovalPanelProps {
-    // 是否打开抽屉
-    open: boolean;
-    // 抽屉关闭回调
-    onClose: () => void;
-    // 抽屉样式类对应
-    drawerClassName?: string;
+  // 是否打开抽屉
+  open: boolean;
+  // 抽屉关闭回调
+  onClose: () => void;
+  // 抽屉样式类对应
+  drawerClassName?: string;
 }
 
-export const ApprovalPanelDrawer: React.FC<ApprovalPanelDrawerProps> = (props) => {
-
-    return (
-        <Drawer
-            rootClassName={props.drawerClassName}
-            open={props.open}
-            mask={false}
-            onClose={props.onClose}
-            styles={{
-                body: {
-                    padding: 0,
-                    margin: 0,
-                }
-            }}
-        >
-            <ApprovalPanel
-                {...props}
-            />
-        </Drawer>
-    )
-}
+export const ApprovalPanelDrawer: React.FC<ApprovalPanelDrawerProps> = (
+  props,
+) => {
+  return (
+    <Drawer
+      push={false}
+      rootClassName={props.drawerClassName}
+      open={props.open}
+      mask={false}
+      onClose={props.onClose}
+      styles={{
+        body: {
+          padding: 0,
+          margin: 0,
+        },
+      }}
+    >
+      <ApprovalPanel {...props} />
+    </Drawer>
+  );
+};
